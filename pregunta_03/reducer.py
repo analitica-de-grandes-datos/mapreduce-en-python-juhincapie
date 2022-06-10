40373611cebfaa5a2065f25729822ce0b89d66fb
juhincapie <juhincapie@unal.edu.co>
2022-06-09 19:53:17 -0500

    Update reducer.py

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        #
+        #
+        lista = line.split(',')
+        #
+        sys.stdout.write("{},{}\n".format(lista[1].strip(),lista[0]))
