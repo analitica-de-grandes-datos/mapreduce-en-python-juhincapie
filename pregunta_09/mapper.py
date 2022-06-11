42cf81bea2f9fa36acfc427e4d9f275642b9b24b
juhincapie <juhincapie@unal.edu.co>
2022-06-11 18:36:08 -0500

    Update mapper.py

--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        lista = line.split('   ')
+        tercera_columna = line.split('   ')[2].strip()
+        tercera_columna = tercera_columna.zfill(4)
+        sys.stdout.write("{}   {}   {}\n".format(
+            tercera_columna, lista[0], lista[1]))
