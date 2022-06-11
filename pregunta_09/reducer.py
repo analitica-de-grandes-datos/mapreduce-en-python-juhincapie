997f3bf9f532d5ba8ad1c0411e383f1b8379634d
juhincapie <juhincapie@unal.edu.co>
2022-06-11 18:36:11 -0500

    Update reducer.py

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    contador = 0
+    for line in sys.stdin:
+        lista = line.split('   ')
+        sys.stdout.write("{}   {}   {}\n".format(
+            lista[1], lista[2].strip(), int(lista[0])))
+        if contador >= 5:
+            break
+        contador += 1
