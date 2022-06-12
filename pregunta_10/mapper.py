02686887c28c57da19626abd5c9620435508bc48
juhincapie <juhincapie@unal.edu.co>
2022-06-11 19:08:38 -0500

    Update mapper.py

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        clave, valor = line.split('\t')
+        valor = list(valor.strip().split(','))
+        clave = clave.zfill(4)
+        for letra in valor:
+            sys.stdout.write("{}\t{}\n".format(letra, clave))
